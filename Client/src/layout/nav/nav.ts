6adfbe3548c86e75bbdb4eb619ae19837c82b913
Everton Ferreira <iseverton2024@gmail.com>
2025-10-25 10:15:42 -0300

    feat(nav): add navegation  routes for nav component

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -3,30 +3,37 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { single, Subscriber } from 'rxjs';
 import { Result } from 'postcss';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
-
   protected creds: any = {};
+
+  private router = inject(Router);
+  private toast = inject(ToastService);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (result) => {
-        console.log(result), (this.creds = {});
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
+        this.creds = {};
       },
       error: (error) => {
-        alert(error);
+        this.toast.error(error.error);
       },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
